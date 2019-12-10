88e9b52808627bd4959fd661fb523d274dc25f5f
Etienne Barassin <etiennebarassin@gmail.com>
2019-12-10 10:41:09 +0100

    boucle for et condition avec modulo

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,1 +1,16 @@
-document.write('Je fais du JS');
+let message = '<ul>';
+
+
+for(let i = -1722 ; i <= 2756 ; i++){
+    if (i % 9 === 0){
+        message += '<li>';
+        message += i;
+        message += '</li>';
+    }
+
+}
+
+
+message += '</ul>';
+alert('ok');
+document.write(message);
